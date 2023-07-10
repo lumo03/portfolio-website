5146bd49690dbb15aff4785df7de9480bc94f608
RiadhAdrani <riadh_adrani@hotmail.fr>
2023-07-10 01:56:55 +0100

    chore: update with master

--- src/lib/experiences.params.ts
+++ src/lib/experiences.params.ts
@@ -4,49 +4,93 @@ import { ContractType, type Experience } from './types';
 
 const MY_EXPERIENCES: Array<Experience> = [
 	{
-		slug: 'open-sourcer',
-		company: 'Self-employed',
-		description: 'Creating awesome tools for developers.',
-		contract: ContractType.SelfEmployed,
+		slug: 'fullstack@hstech',
+		name: 'Fullstack Developer',
+		company: 'HSTECH',
+		description: '',
+		contract: ContractType.FullTime,
 		type: 'Software Development',
-		location: 'Home',
-		period: { from: new Date() },
-		skills: getSkills('ts', 'js'),
-		name: 'Open Source Developer',
-		color: '#ffffff',
+		location: 'Remote, Tunisia',
+		color: 'white',
 		links: [],
 		logo: Assets.Unknown,
-		shortDescription: ''
+		period: { from: new Date(2022, 9, 3) },
+		skills: getSkills(
+			'deno',
+			'ts',
+			'node',
+			'svelte',
+			'quasar',
+			'nuxt',
+			'vue',
+			'reactjs',
+			'sass',
+			'tailwind',
+			'redis',
+			'mongodb',
+			'fastify',
+			'docker'
+		)
 	},
 	{
-		slug: 'software-freelance',
+		slug: 'software-freelancer',
+		name: 'Software Development Freelancer',
 		company: 'Self-employed',
-		description: 'Creating awesome applications for customers.',
+		description:
+			'Creating Mobile and Web Apps for customers using various technologies like "Xamarin", "Native Android", "React" ...',
 		contract: ContractType.Freelance,
 		type: 'Software Development',
-		location: 'Home',
-		period: { from: new Date() },
-		skills: getSkills('svelte', 'ts', 'sass', 'css', 'html', 'js'),
-		name: 'Freelancer',
-		color: '#ffffff',
+		color: 'white',
 		links: [],
 		logo: Assets.Unknown,
-		shortDescription: ''
+		location: 'Tunisia',
+		period: { from: new Date(2021, 10, 1), to: new Date(2022, 5, 16) },
+		skills: getSkills('js', 'node', 'reactjs', 'kotlin', 'firebase', 'xamarin', 'csharp')
 	},
 	{
-		slug: 'software-freelance-junior',
-		company: 'Self-employed',
-		description: 'Creating awesome applications for customers.',
-		contract: ContractType.Freelance,
-		type: 'Software Development',
-		location: 'Home',
-		period: { from: new Date(2022, 0, 1), to: new Date() },
-		skills: getSkills('css', 'html', 'js'),
-		name: 'Junior Freelancer',
-		color: '#ffffff',
+		slug: 'web-designer@triwen',
+		name: 'Web Designer',
+		company: 'TriWeb',
+		description:
+			'My job consisted of designing (Using Adobe Photoshop and Illustrator) the graphical charter of turnkey websites and integrating them with the help of a custom CMS made for TriWeb.',
+		contract: ContractType.FullTime,
+		type: 'UI Design',
+		location: 'Tunis, Tunisia',
+		period: { from: new Date(2021, 4, 1), to: new Date(2021, 11, 12) },
+		color: 'white',
 		links: [],
 		logo: Assets.Unknown,
-		shortDescription: ''
+		skills: getSkills('html', 'css', 'js', 'ps', 'ai')
+	},
+	{
+		slug: 'graphic-designer-freelancer',
+		name: 'Graphic Designer',
+		company: 'Self-employed',
+		color: 'white',
+		links: [],
+		logo: Assets.Unknown,
+		description:
+			'Designing graphics for small startups and companies, Creating logos, posters ... etc',
+		contract: ContractType.Freelance,
+		type: 'Graphic Design',
+		location: 'Tunisia',
+		period: { from: new Date(2020, 5, 1), to: new Date(2021, 11, 12) },
+		skills: getSkills('ps', 'ae', 'ai', 'pp')
+	},
+	{
+		slug: 'intern-brain-academy',
+		name: 'Intern',
+		company: 'Brain Academy',
+		color: 'white',
+		links: [],
+		logo: Assets.Unknown,
+		description:
+			'We developed the prototype of "SMARTchair" which is an EEG controlled wheelchair. Using our skills was not enough, so it was imminent for us to learn and get used to many other programming languages, software and even new operating systems. The project was defended the 19th of July 2020 and got "Very Good" as honor.',
+		contract: ContractType.Internship,
+		type: 'Robotics',
+		location: 'Ben Arous, Tunisia',
+		period: { from: new Date(2020, 0, 1), to: new Date(2020, 6, 19) },
+		skills: getSkills('python')
 	}
 ];
 
--- src/lib/params.ts
+++ src/lib/params.ts
@@ -14,7 +14,7 @@ import {
 } from './types';
 import { Icons } from './utils';
 
-export const TITLE_SUFFIX = 'Slick template with Svelte';
+export const TITLE_SUFFIX = 'Riadh Adrani';
 
 export const NavBar = {
 	home: 'Home',
@@ -46,35 +46,18 @@ export const getPlatfromIcon = (platform: Platform): Icons => {
 
 export const HOME: HomePageParams = {
 	title: 'Home',
-	name: 'Name',
-	lastName: 'LASTNAME',
-	description:
-		'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corrupti, rerum. Debitis accusantium deleniti enim iste dignissimos? Similique, exercitationem! Odit vero, numquam quae ratione maxime sunt reiciendis laudantium quaerat iure ipsum!',
+	name: 'Riadh',
+	lastName: 'Adrani',
+	description: 'A self-made software engineer',
 	links: [
-		{ platform: Platform.GitHub, link: 'https://github.com/' },
+		{ platform: Platform.GitHub, link: 'https://github.com/RiadhAdrani' },
 		{
 			platform: Platform.Linkedin,
-			link: 'https://www.linkedin.com/'
-		},
-		{
-			platform: Platform.Twitter,
-			link: 'https://twitter.com/'
+			link: 'https://www.linkedin.com/in/riadh-adrani/'
 		},
 		{
 			platform: Platform.StackOverflow,
-			link: 'https://stackoverflow.com/'
-		},
-		{
-			platform: Platform.Email,
-			link: 'riadh-adrani@hotmail.fr'
-		},
-		{
-			platform: Platform.Youtube,
-			link: 'https://www.youtube.com'
-		},
-		{
-			platform: Platform.Facebook,
-			link: 'https://www.facebook.com'
+			link: 'hhttps://stackoverflow.com/users/12978534/riadh-adrani'
 		}
 	]
 };
